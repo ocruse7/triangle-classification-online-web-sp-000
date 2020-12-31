1185cea7070a9db05b51e8eef4ae5c848aaf6db6
Owen Cruse <ocruse7@gmail.com>
2020-12-31 22:51:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,28 +1,28 @@
 class Triangle
-  attr_accessor :side_1, :side_2, :side_3
-
-  def initialize(side_1, side_2, side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    is_triangle
-    if side_1 == side_2 && side_2 == side_3
+    validate_triangle
+    if a == b && b == c
       :equilateral
-    elsif side_1 == side_2 || side_2 == side_3 || side_3 == side_1
+    elsif a == b || b == c || a == c
       :isosceles
     else
       :scalene
     end
   end
 
-  def is_triangle
-    triangle = [(side_1 + side_2 > side_3), (side_1 + side_3 > side_2), (side_2 + side_3 > side_1)]
-    [side_1, side_2, side_3].each { |side| triangle << false if side <= 0 }
-    raise TriangleError if triangle.include?(false)
-    end
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
   end
+
   class TriangleError < StandardError
+  end
 end
